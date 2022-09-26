2f5ff7d9fab4e703ad80a431f7a4c6882f423eed
Tornike Jandieri <108745129+tornikejandieri@users.noreply.github.com>
2022-09-26 20:20:36 +0400

    route path with Id

--- src/main.js
+++ src/main.js
@@ -5,12 +5,13 @@ import App from './App.vue';
 //import components that router will use
 import TeamsList from './components/teams/TeamsList.vue';
 import UsersList from './components/users/UsersList.vue';
-
+import TeamMembers from './components/teams/TeamMembers.vue';
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: '/teams', component: TeamsList }, //create endpoint for links
     { path: '/users', component: UsersList },
+    { path: '/teams/:teamId', component: TeamMembers },
   ],
   linkActiveClass: 'active',
 });
